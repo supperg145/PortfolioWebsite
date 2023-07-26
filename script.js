63f5896e22d5c866cbdf42ef8d7f00ed90d7382c
supperg <gabor1415@gmail.com>
2023-07-26 11:11:18 +0200

    Added more functionality anmd styling

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
-        // Get the img object using its Id
-        img = document.getElementById("img1");
-        // Function to increase image size
-        function enlargeImg() {
-            // Set image size to 1.5 times original
-            img.style.transform = "scale(3)";
-            // Animation effect
-            img.style.transition = "transform 0.25s ease";
-        }
-        // Function to reset image size
-        function resetImg() {
-            // Set image size to original
-            img.style.transform = "scale(1)";
-            img.style.transition = "transform 0.25s ease";
-        }
+// Function to toggle image size
+function toggleImgSize(imgElement) {
+    // Check if the image is currently enlarged
+    if (imgElement.classList.contains("enlarged")) {
+        // If enlarged, reset size to original
+        imgElement.style.transform = "scale(1)";
+        imgElement.style.zIndex = "1";
+    } else {
+        // If not enlarged, set image size to 2 times original
+        imgElement.style.transform = "scale(2)";
+        imgElement.style.zIndex = "5";
+    }
+
+    // Toggle the 'enlarged' class on the image
+    imgElement.classList.toggle("enlarged");
+
+    // Animation effect
+    imgElement.style.transition = "transform 0.25s ease";
+}
+
+// Get all images with the class name 'enlarge-clickable'
+const images = document.querySelectorAll(".enlarge-clickable");
+
+// Add click event listeners to all images with the 'enlarge-clickable' class
+images.forEach((img) => {
+    img.addEventListener("click", () => toggleImgSize(img));
+});
